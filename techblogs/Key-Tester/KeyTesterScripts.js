86fbaf80f75c38be8e34776d4db0b1a937e80adc
Hello9999901 <61983584+Hello9999901@users.noreply.github.com>
2021-01-19 09:13:29 -0500

    keytester

---
+++ techblogs/Key-Tester/KeyTesterScripts.js
@@ -0,0 +1,472 @@
+'use strict';
+
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (keyName === 'Escape') {
+    document.getElementById("Escape").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F1') {
+    document.getElementById("F1").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F2') {
+    document.getElementById("F2").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F3') {
+    document.getElementById("F3").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F4') {
+    document.getElementById("F4").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F5') {
+    document.getElementById("F5").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F6') {
+    document.getElementById("F6").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F7') {
+    document.getElementById("F7").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F8') {
+    document.getElementById("F8").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F9') {
+    document.getElementById("F9").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F10') {
+    document.getElementById("F10").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F11') {
+    document.getElementById("F11").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'F12') {
+    document.getElementById("F12").style.backgroundColor = "Green";
+  }
+  else if (keyName === '`') {
+    document.getElementById("`").style.backgroundColor = "Green";
+  }
+  else if (keyName === '1') {
+    document.getElementById("1").style.backgroundColor = "Green";
+  }
+  else if (keyName === '2') {
+    document.getElementById("2").style.backgroundColor = "Green";
+  }
+  else if (keyName === '3') {
+    document.getElementById("3").style.backgroundColor = "Green";
+  }
+  else if (keyName === '4') {
+    document.getElementById("4").style.backgroundColor = "Green";
+  }
+  else if (keyName === '5') {
+    document.getElementById("5").style.backgroundColor = "Green";
+  }
+  else if (keyName === '6') {
+    document.getElementById("6").style.backgroundColor = "Green";
+  }
+  else if (keyName === '7') {
+    document.getElementById("7").style.backgroundColor = "Green";
+  }
+  else if (keyName === '8') {
+    document.getElementById("8").style.backgroundColor = "Green";
+  }
+  else if (keyName === '9') {
+    document.getElementById("9").style.backgroundColor = "Green";
+  }
+  else if (keyName === '0') {
+    document.getElementById("0").style.backgroundColor = "Green";
+  }
+  else if (keyName === '-') {
+    document.getElementById("-").style.backgroundColor = "Green";
+  }
+  else if (keyName === '=') {
+    document.getElementById("=").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'Backspace') {
+    document.getElementById("Backspace").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'Tab') {
+    document.getElementById("Tab").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'q') {
+    document.getElementById("q").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'w') {
+    document.getElementById("w").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'e') {
+    document.getElementById("e").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'r') {
+    document.getElementById("r").style.backgroundColor = "Green";
+  }
+  else if (keyName === 't') {
+    document.getElementById("t").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'y') {
+    document.getElementById("y").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'u') {
+    document.getElementById("u").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'i') {
+    document.getElementById("i").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'o') {
+    document.getElementById("o").style.backgroundColor = "Green";
+  }
+  else if (keyName === 'p') {
+    document.getElementById("p").style.backgroundColor = "Green";
+  }
+  else if (keyName === '[') {
+    document.getElementById("[").style.backgroundColor = "Green";
+  }
+  else if (keyName === ']') {
+    document.getElementById("]").style.backgroundColor = "Green";
+  }
+  else if (keyName === "\\") {
+    document.getElementById("\\").style.backgroundColor = "Green";
+  }
+  else if (keyName === "CapsLock") {
+    document.getElementById("CapsLock").style.backgroundColor = "Green";
+  }
+  else if (keyName === "a") {
+    document.getElementById("a").style.backgroundColor = "Green";
+  }
+  else if (keyName === "s") {
+    document.getElementById("s").style.backgroundColor = "Green";
+  }
+  else if (keyName === "d") {
+    document.getElementById("d").style.backgroundColor = "Green";
+  }
+  else if (keyName === "f") {
+    document.getElementById("f").style.backgroundColor = "Green";
+  }
+  else if (keyName === "g") {
+    document.getElementById("g").style.backgroundColor = "Green";
+  }
+  else if (keyName === "h") {
+    document.getElementById("h").style.backgroundColor = "Green";
+  }
+  else if (keyName === "j") {
+    document.getElementById("j").style.backgroundColor = "Green";
+  }
+  else if (keyName === "k") {
+    document.getElementById("k").style.backgroundColor = "Green";
+  }
+  else if (keyName === "l") {
+    document.getElementById("l").style.backgroundColor = "Green";
+  }
+  else if (keyName === ";") {
+    document.getElementById(";").style.backgroundColor = "Green";
+  }
+  else if (keyName === "'") {
+    document.getElementById("'").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Enter") {
+    document.getElementById("Enter").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Shift") {
+    document.getElementById("Shift").style.backgroundColor = "Green";
+    document.getElementById("Shift2").style.backgroundColor = "Green";
+  }
+  else if (keyName === "z") {
+    document.getElementById("z").style.backgroundColor = "Green";
+  }
+  else if (keyName === "x") {
+    document.getElementById("x").style.backgroundColor = "Green";
+  }
+  else if (keyName === "c") {
+    document.getElementById("c").style.backgroundColor = "Green";
+  }
+  else if (keyName === "v") {
+    document.getElementById("v").style.backgroundColor = "Green";
+  }
+  else if (keyName === "b") {
+    document.getElementById("b").style.backgroundColor = "Green";
+  }
+  else if (keyName === "n") {
+    document.getElementById("n").style.backgroundColor = "Green";
+  }
+  else if (keyName === "m") {
+    document.getElementById("m").style.backgroundColor = "Green";
+  }
+  else if (keyName === ",") {
+    document.getElementById(",").style.backgroundColor = "Green";
+  }
+  else if (keyName === ".") {
+    document.getElementById(".").style.backgroundColor = "Green";
+  }
+  else if (keyName === "/") {
+    document.getElementById("/").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Control") {
+    document.getElementById("Control").style.backgroundColor = "Green";
+    document.getElementById("Control2").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Alt") {
+    document.getElementById("Alt").style.backgroundColor = "Green";
+    document.getElementById("Alt2").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Meta") {
+    document.getElementById("Meta").style.backgroundColor = "Green";
+    document.getElementById("Meta2").style.backgroundColor = "Green";
+  }
+  else if (keyName === " ") {
+    document.getElementById("Space").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Meta") {
+    document.getElementById("Meta").style.backgroundColor = "Green";
+  }
+  else if (keyName === "Alt") {
+    document.getElementById("Alt").style.backgroundColor = "Green";
+  }
+  else if (keyName === "ArrowLeft") {
+    document.getElementById("ArrowLeft").style.backgroundColor = "Green";
+  }
+  else if (keyName === "ArrowRight") {
+    document.getElementById("ArrowRight").style.backgroundColor = "Green";
+  }
+  else if (keyName === "ArrowUp") {
+    document.getElementById("ArrowUp").style.backgroundColor = "Green";
+  }
+  else if (keyName === "ArrowDown") {
+    document.getElementById("ArrowDown").style.backgroundColor = "Green";
+  }
+  else {
+    // alert(`test${keyName}test`);
+  }
+}, false);
+
+document.addEventListener('keyup', (event) => {
+  const keyName = event.key;
+  if (keyName === 'Escape') {
+    document.getElementById("Escape").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F1') {
+    document.getElementById("F1").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F2') {
+    document.getElementById("F2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F3') {
+    document.getElementById("F3").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F4') {
+    document.getElementById("F4").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F5') {
+    document.getElementById("F5").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F6') {
+    document.getElementById("F6").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F7') {
+    document.getElementById("F7").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F8') {
+    document.getElementById("F8").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F9') {
+    document.getElementById("F9").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F10') {
+    document.getElementById("F10").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F11') {
+    document.getElementById("F11").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'F12') {
+    document.getElementById("F12").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '`') {
+    document.getElementById("`").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '1') {
+    document.getElementById("1").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '2') {
+    document.getElementById("2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '3') {
+    document.getElementById("3").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '4') {
+    document.getElementById("4").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '5') {
+    document.getElementById("5").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '6') {
+    document.getElementById("6").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '7') {
+    document.getElementById("7").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '8') {
+    document.getElementById("8").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '9') {
+    document.getElementById("9").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '0') {
+    document.getElementById("0").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '-') {
+    document.getElementById("-").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '=') {
+    document.getElementById("=").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'Backspace') {
+    document.getElementById("Backspace").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'Tab') {
+    document.getElementById("Tab").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'q') {
+    document.getElementById("q").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'w') {
+    document.getElementById("w").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'e') {
+    document.getElementById("e").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'r') {
+    document.getElementById("r").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 't') {
+    document.getElementById("t").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'y') {
+    document.getElementById("y").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'u') {
+    document.getElementById("u").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'i') {
+    document.getElementById("i").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'o') {
+    document.getElementById("o").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === 'p') {
+    document.getElementById("p").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === '[') {
+    document.getElementById("[").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === ']') {
+    document.getElementById("]").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "\\") {
+    document.getElementById("\\").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "CapsLock") {
+    document.getElementById("CapsLock").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "a") {
+    document.getElementById("a").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "s") {
+    document.getElementById("s").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "d") {
+    document.getElementById("d").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "f") {
+    document.getElementById("f").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "g") {
+    document.getElementById("g").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "h") {
+    document.getElementById("h").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "j") {
+    document.getElementById("j").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "k") {
+    document.getElementById("k").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "l") {
+    document.getElementById("l").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === ";") {
+    document.getElementById(";").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "'") {
+    document.getElementById("'").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Enter") {
+    document.getElementById("Enter").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Shift") {
+    document.getElementById("Shift").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+    document.getElementById("Shift2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "z") {
+    document.getElementById("z").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "x") {
+    document.getElementById("x").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "c") {
+    document.getElementById("c").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "v") {
+    document.getElementById("v").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "b") {
+    document.getElementById("b").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "n") {
+    document.getElementById("n").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "m") {
+    document.getElementById("m").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === ",") {
+    document.getElementById(",").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === ".") {
+    document.getElementById(".").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "/") {
+    document.getElementById("/").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Control") {
+    document.getElementById("Control").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+    document.getElementById("Control2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Alt") {
+    document.getElementById("Alt").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+    document.getElementById("Alt2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Meta") {
+    document.getElementById("Meta").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+    document.getElementById("Meta2").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === " ") {
+    document.getElementById("Space").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Meta") {
+    document.getElementById("Meta").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "Alt") {
+    document.getElementById("Alt").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "ArrowLeft") {
+    document.getElementById("ArrowLeft").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "ArrowRight") {
+    document.getElementById("ArrowRight").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "ArrowUp") {
+    document.getElementById("ArrowUp").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+  else if (keyName === "ArrowDown") {
+    document.getElementById("ArrowDown").style.backgroundColor = "rgba(106, 164, 61, 0.5)";
+  }
+}, false);
