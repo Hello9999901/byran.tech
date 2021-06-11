149c23648819b3b34d898b4ca0ddb264d827f7fa
Hello9999901 <byran8855@gmail.com>
2021-06-11 12:16:23 -0400

    erase

--- cookies.js
+++
@@ -1,8 +0,0 @@
-if(localStorage.getItem('cookieSeen') != 'shown'){
-    $(".cookie-banner").delay(2000).fadeIn();
-    localStorage.setItem('cookieSeen','shown')
-}
-
-$('.close').click(function(e) {
-  $('.cookie-banner').fadeOut(); 
-});
