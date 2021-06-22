b9cb2795df58b05f3c9df715e2d63ee3ff35eb08
Hello9999901 <byran8855@gmail.com>
2021-06-21 21:51:24 -0400

    redesigned art

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ var typed = new Typed('.typed', {
     typeSpeed: 35,
     startDelay: 750,
 });
+
+function showOverlays() {
+    $(".overlay").addClass("showOverlay");
+    $(".description").addClass("showOverlay");
+    $("hr").addClass("showOverlay");
+};
+
+function hideOverlays() {
+    $(".overlay").removeClass("showOverlay");
+    $(".description").removeClass("showOverlay");
+    $("hr").removeClass("showOverlay");
+};
