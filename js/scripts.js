7824168601741e004189f11cebfe4d86ae79f9ee
Hello9999901 <byran8855@gmail.com>
2021-07-08 15:55:17 -0400

    added cookies

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,35 @@ function hideOverlays() {
 };
 
 document.getElementById("currentTimeAndDate").innerHTML = (Date().toLocaleString());
+
+function getCookieValue() {
+    if (document.cookie.split(';').some((item) => item.includes('cookieStatus=accepted'))) {
+        document.getElementById("cookieBanner").style.visibility = 'hidden';
+    }
+    else if (document.cookie.split(';').some((item) => item.includes('cookieStatus=denied'))) {
+        document.getElementById("cookieBanner").style.visibility = 'hidden';
+    }
+    else {
+        document.getElementById("cookieBanner").style.visibility = 'visible';
+    }
+}
+
+function onloadFunction() {
+    getCookieValue()
+}
+
+window.onload = onloadFunction();
+
+function acceptCookies() {
+    document.cookie = "cookieStatus=accepted; max-age=604800";
+    document.getElementById("cookieBanner").style.visibility = 'hidden';
+    gtag('consent', 'update', {
+        'ad_storage': 'denied',
+        'analytics_storage': 'granted',
+    });
+}
+
+function deniedCookies() {
+    document.cookie = "cookieStatus=denied; max-age=604800";
+    document.getElementById("cookieBanner").style.visibility = 'hidden';
+}
