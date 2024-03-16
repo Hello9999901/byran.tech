fcf6acb1ae95da6fabf87bf021a3a621a13a8e5c
Hello9999901 <byran8855@gmail.com>
2024-03-15 23:45:32 -0400

    new website wip!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), react(), partytown()]
 });
